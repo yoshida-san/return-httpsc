3a7989428a0af9650c7a896ba7ddd07a4f62204b
s-yoshida <yos.3104@gmail.com>
2018-12-17 14:15:33 +0900

    modify

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
-
 try {
-    $ec = filter_input(INPUT_GET, 'ec', FILTER_SANITIZE_NUMBER_INT) * 1;
-    http_response_code($ec);
-    echo 'http status: ', http_response_code();
+    http_response_code(filter_input(INPUT_GET, 'ec', FILTER_SANITIZE_NUMBER_INT) * 1);
 } catch (Exception $e) {
     echo 'error: ',  $e->getMessage();
 }
